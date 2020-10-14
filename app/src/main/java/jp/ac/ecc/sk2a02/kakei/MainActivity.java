35fe353b0311649498183cd4448b6e41a2608610
unknown <2180034@s2180034.maetel.ecc.ac.jp>
2020-10-14 10:31:27 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/ecc/sk2a02/kakei/MainActivity.java
@@ -0,0 +1,29 @@
+package jp.ac.ecc.sk2a02.kakei;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
